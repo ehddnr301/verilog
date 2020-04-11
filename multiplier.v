6149e3b373dd6f424d6563f5410df09d88c5c681
ehddnr301 <dy950328@gmail.com>
2020-04-11 21:23:19 +0900

    2

--- multiplier.v
+++
@@ -1,8 +0,0 @@
-module Multiplier (Op1, Op2, Result);
-    parameter EM = 4, EN = 2;
-    input [EM:1] Op1;
-    input [EN:1] Op2;
-    output [EM+EN:1] Result;
-    assign Result = Op1 * Op2;
-
-endmodule
